362330ffa328423fd2636108cc5158b7d3e2ac23
MathCoimbra <matheuscoimbra35@gmail.com>
2022-06-29 15:52:54 -0300

    melhorando explicação de alguns códigos

--- Referenciar javascript no HTML/js_nao_obstrutivo.js
+++ Referenciar javascript no HTML/js_nao_obstrutivo.js
@@ -1,8 +1,8 @@
 /*window.onload - utilizado para que todo o código JS rode somente quando o HTML for renderizado*/
 window.onload = function () {
-  /* Objeto botao do HTML é transformado, no arquivo JS, em uma variável através da função document.getElementById()*/
+  /* Para que ocorra a execução do alerta por meio do botão é necessário utilizar o DOM e identificar o objeto do botão, o mesmo deverá receber o atributo ID no HTML, e logo chamá-lo via função document.getElementById() - essa função chama o ID do objeto e em caso positivo retorna o objeto correspondente.*/
   var objBotao = document.getElementById('botao')
-  /*Depois essa variável é alocada a classe/atributo onclick que logo é transformada em uma função*/
+  /*Depois essa variável (objBotao) é alocada a classe/atributo onclick que logo é transformada em uma função*/
   objBotao.onclick = function () {
     window.alert('Alô, Mundo!')
   }
